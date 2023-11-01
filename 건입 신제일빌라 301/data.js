56fc353f8898f1c2b8eee975b7893b871db12cb8
jeju-foryou <task0135@naver.com>
2023-11-01 14:51:54 +0900

    신제일빌라 VR룸 업로드

---
+++ 건입 신제일빌라 301/data.js
@@ -0,0 +1,507 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vr--",
+      "name": "vr 룸 입장",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23327637611674135,
+          "pitch": 0.2045019683478575,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "현관",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7541436687671723,
+          "pitch": -0.001291578779039071,
+          "rotation": 4.71238898038469,
+          "target": "3-_2"
+        },
+        {
+          "yaw": -0.26566157634368714,
+          "pitch": 0.07265390704101016,
+          "rotation": 6.283185307179586,
+          "target": "4-_"
+        },
+        {
+          "yaw": 0.13225269767778336,
+          "pitch": -0.043205581745128896,
+          "rotation": 7.853981633974483,
+          "target": "2-_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-_1",
+      "name": "작은방_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.4007155440645525,
+        "pitch": 0.03935394968129735,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3410586704092804,
+          "pitch": 0.0952562439883664,
+          "rotation": 0,
+          "target": "4-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-_2",
+      "name": "작은방_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.625519239603099,
+        "pitch": 0.029983961661942615,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5403696757913394,
+          "pitch": 0.05122581934512027,
+          "rotation": 0,
+          "target": "4-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-_",
+      "name": "거실_복도",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5475199701007956,
+          "pitch": 0.1343005118982532,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.989299885644453,
+          "pitch": -0.03695962308583134,
+          "rotation": 1.5707963267948966,
+          "target": "3-_2"
+        },
+        {
+          "yaw": 1.5224168064030703,
+          "pitch": -0.02102042709367069,
+          "rotation": 0,
+          "target": "2-_1"
+        },
+        {
+          "yaw": 0.4694443057025026,
+          "pitch": 0.006748758554515177,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.5465800431300618,
+          "pitch": 0.03153967958672865,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "거실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.845202577447937,
+        "pitch": 0.009369988019356512,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2615976679349927,
+          "pitch": -0.022444823092445887,
+          "rotation": 1.5707963267948966,
+          "target": "8-"
+        },
+        {
+          "yaw": -2.7991074445070545,
+          "pitch": -0.054276298805776335,
+          "rotation": 1.5707963267948966,
+          "target": "7-"
+        },
+        {
+          "yaw": -3.026908020655636,
+          "pitch": 0.09140274346292188,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 1.9802047055052956,
+          "pitch": 0.06684161343547501,
+          "rotation": 0,
+          "target": "4-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "주방",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.1136364514300077,
+        "pitch": -0.020036876087448974,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8471559676987841,
+          "pitch": 0.009369988019356512,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -3.088619160904006,
+          "pitch": 0.04578393326372776,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.3934887996129532,
+          "pitch": 0.0030462267883351046,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "세탁실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.123664159556764,
+        "pitch": 0.08973141467948231,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.06323626586316,
+          "pitch": 0.023159950507945837,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "안방",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.79813782397361,
+        "pitch": 0.04787676185710943,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2348239119455044,
+          "pitch": 0.05518832765532622,
+          "rotation": 4.71238898038469,
+          "target": "5-"
+        },
+        {
+          "yaw": 3.0954284019721463,
+          "pitch": 0.028648514843478523,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "드레스룸",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47017143174077525,
+          "pitch": 0.013844996689856615,
+          "rotation": 0,
+          "target": "10-_"
+        },
+        {
+          "yaw": 0.5590043768573576,
+          "pitch": 0.05033928087560291,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-_",
+      "name": "화장실_안방",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.6544799465173465,
+        "pitch": 0.03593665993306594,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5493771623093426,
+          "pitch": 0.06834925941716818,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "신제일빌라 301호",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ 건입 신제일빌라 501/data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vr--",
+      "name": "vr 룸 입장",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23537735902293022,
+          "pitch": 0.2085394490640038,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "거실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.1137490925843636,
+        "pitch": 0.06371215248654671,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.457180179594552,
+          "pitch": -0.11451296778209752,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.8106167547061585,
+          "pitch": 0.0694026490800006,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.7323619997549402,
+          "pitch": 0.01463742577178806,
+          "rotation": 1.5707963267948966,
+          "target": "2-_"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1234175286233707,
+          "pitch": 0.06452111705333508,
+          "title": "작은방",
+          "text": "작은방"
+        }
+      ]
+    },
+    {
+      "id": "2-_",
+      "name": "화장실_거실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.574732899814304,
+        "pitch": 0.08548219782729305,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.282903092058244,
+          "pitch": -0.01039490076019689,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7387574644437453,
+          "pitch": 0.07906620798056352,
+          "title": "세탁실 및 발코니",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">세탁실 및 발코니</span><br>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "안방",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.176734935976377,
+        "pitch": 0.13295479539555188,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9126755157033148,
+          "pitch": 0.07663841698196272,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.2900252442044007,
+          "pitch": 0.03735683216562258,
+          "title": "안방 화장실",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">안방 화장실</span>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "복층",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8028520062354598,
+        "pitch": 0.20988773163357344,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4737496068324827,
+          "pitch": 0.12130573712008541,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.4000792073330146,
+          "pitch": 0.09834795064440804,
+          "rotation": 4.71238898038469,
+          "target": "5-_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-_",
+      "name": "복층_발코니",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.0341299891305873,
+        "pitch": 0.06488743429623511,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.118482764492537,
+          "pitch": 0.06822078476716342,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "신제일빌라 501호",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
